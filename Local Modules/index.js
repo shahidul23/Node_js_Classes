c31aec5fd09e9ead28ab3352a2639e1a1af3c0a0
Shahidul Islam <96639022+shahidul23@users.noreply.github.com>
2023-03-07 17:32:09 +0600

    fristClass

---
+++ Local Modules/index.js
@@ -0,0 +1,8 @@
+//All module use in one Varible
+//const s1 = require('./student');
+//only one function get
+const {getName} = require('./student');
+
+//console.log(s1.cgpa);
+console.log(getName());
+//console.log(s1.getAge());
---
+++ Local Modules/student.js
@@ -0,0 +1,18 @@
+exports.getName = ()=>{
+    return "Shahidul Islam";
+}
+const getAge = ()=>{
+    return "34";
+}
+
+const cgpa = 3.13;
+
+// exports.name = getName;
+// exports.age = getAge;
+// exports.getCgpa = cgpa;
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     cgpa
+// }
